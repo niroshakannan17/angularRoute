77f2685e5695c6720962f106540cad2797c598ab
nirosha <niroshakannan17@gmail.com>
2024-04-13 17:50:53 +0530

    router module and router outlet, router link configure

--- app-route/src/app/app.module.ts
+++ app-route/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -6,6 +6,13 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactUsComponent } from './contact-us/contact-us.component';
+import { RouterModule,Routes } from '@angular/router';
+
+const appRoutes:Routes = [
+  { path:'', component:HomeComponent},
+  { path:'about', component:AboutComponent},
+  { path:'contact-us', component:ContactUsComponent}
+]
 
 @NgModule({
   declarations: [
@@ -16,7 +23,9 @@ import { ContactUsComponent } from './contact-us/contact-us.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
